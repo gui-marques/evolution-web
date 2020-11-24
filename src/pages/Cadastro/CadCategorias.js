4eb1fee242c2119d4520026904cc78271cd6b20a
Guilherme <gui.slayon@live.com>
2020-11-24 16:56:14 -0300

    second commit

--- src/pages/Cadastro/CadCategorias.js
+++ src/pages/Cadastro/CadCategorias.js
@@ -7,7 +7,7 @@ import Box from '@material-ui/core/Box';
 
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+
 
 
 import MenuAdmin from '../../components/MenuAdmin/menu-admin.js'
